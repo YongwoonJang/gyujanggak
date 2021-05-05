1f3d1d6fc931418aaa362f491e3be54a4be72b65
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-05-06 00:31:26 +0900

    Modify : add new line

--- app/nextjs-blog/pages/index.js
+++ app/nextjs-blog/pages/index.js
@@ -39,6 +39,7 @@ export default function Home({ allPostsData }) {
               <Link href={url}>
                 <a>Go to original site</a>
               </Link>
+              <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small> 
