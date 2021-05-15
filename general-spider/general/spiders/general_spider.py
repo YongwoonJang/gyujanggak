b8a65326094ddfe9756b3204ce3f042850cf2f77
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-05-16 02:08:28 +0900

    Modify : for crawling datta and push data to 'es'

--- general-spider/general/spiders/general_spider.py
+++ general-spider/general/spiders/general_spider.py
@@ -48,7 +48,7 @@ class GeneralSpider(Spider):
 
             contents_key_value["주요내용"] = ",".join(filtered_contents)
 
-            # 목적
+            # 목적 아래 위치에서만 아래 형식으로 데이터 확인이 가능함.
             if "https://www.assembly.go.kr/assm/assemact/council/council0101/assmSchCal/assemScanCalDetail.do" in kwargs["url"] :
                 
                 contents_key_value["목적"] = soup.find_all("tr","general")[0].contents[1].contents[1].contents[0]
@@ -57,7 +57,8 @@ class GeneralSpider(Spider):
                     contents_key_value[subject_html[0].find_all("th")[i].text.strip()] = subject_html[0].find_all("td")[i].text.strip()
                 contents_key_value[subject_html[1].a.text] = subject_html[1].a['href']
 
-            print("===== session =======")
-            print(contents_key_value)
-            es = Elasticsearch("es01:9200")
+            # AWS 향으로 변경해 주어야함.
+            es = Elasticsearch(["https://gyujanggak-deployment.es.us-east-1.aws.found.io:9243"])
             res = es.index(index='national-index', body=contents_key_value)
+            print("*************")
+            print(res)
--- general-spider/utility.sh
+++ general-spider/utility.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-DIR="/code/frontera/examples/general-spider" 
+DIR="/code/general-spider" 
 
 cd $DIR
 
@@ -16,6 +16,7 @@ clean_up() {
 	rm -rf nohup.out
 	rm -rf ./url.json
 	rm -rf ./utility.log
+	rm -rf ./log/*.log
 	exit
 
 }
@@ -27,26 +28,26 @@ echo "Check whether processes need to be start"
 	if [[ ($(ps -f | grep "frontera" | awk '{print NF}' | wc -l) -eq 1 && $(ps -f | grep "crawl" | awk '{print NF}' | wc -l) -eq 1) ]]; then 
 		echo "It is needed, Processes are starting ..."
 
-		nohup python3 -m frontera.contrib.messagebus.zeromq.broker 0>/code/frontera/examples/general-spider/log/zeromq.log 1>&0 2>&0 &
+		nohup python3 -m frontera.contrib.messagebus.zeromq.broker 0>/code/general-spider/log/zeromq.log 1>&0 2>&0 &
 		sleep 3
 
-		nohup python3 -m frontera.utils.add_seeds --config config.dbw --seeds-file seeds_es_smp.txt
+		nohup python3 -m frontera.utils.add_seeds --config config.dbw --seeds-file national_assembly_sites.txt
 		sleep 3
 
-		nohup python3 -m frontera.worker.strategy --config config.sw 0>/code/frontera/examples/general-spider/log/strategy.log 1>&0 2>&0 &
+		nohup python3 -m frontera.worker.strategy --config config.sw 0>/code/general-spider/log/strategy.log 1>&0 2>&0 &
 		sleep 3 
 
-		nohup python3 -m scrapy crawl general 0>/code/frontera/examples/general-spider/log/crawl.log 1>&0 2>&0 &
+		nohup python3 -m scrapy crawl general 0>/code/general-spider/log/crawl.log 1>&0 2>&0 &
 		sleep 3 
 
-		nohup python3 -m frontera.worker.db --no-incoming --config config.dbw --partitions 0 0>/code/frontera/examples/general-spider/log/db.log 1>&0 2>&0 &
+		nohup python3 -m frontera.worker.db --no-incoming --config config.dbw --partitions 0 0>/code/general-spider/log/db.log 1>&0 2>&0 &
 		sleep 3 
 
 		echo "Processes are started ..."
 	fi
 
 echo "End of check"
-	if [[ (-f ${DIR}/log/crawl.log  ]]; then
+	if [[ (-f ${DIR}/log/crawl.log)  ]]; then
 		echo "Start line of /log/crawl.log"
 		echo "Last line of /log/crawl.log"
 		echo $(tail -1 ${DIR}/log/crawl.log)
