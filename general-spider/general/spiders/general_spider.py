0a471779fce8d058e80261c16549243af250f9e0
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-05-17 23:16:22 +0900

    Modify : Gear enterprise search with scrapy

--- general-spider/general/spiders/general_spider.py
+++ general-spider/general/spiders/general_spider.py
@@ -5,10 +5,11 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.exporters import JsonLinesItemExporter
 from scrapy import Item
 from scrapy import Field
-from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
 import requests
 import json
+import configparser
+from elastic_enterprise_search import AppSearch
 
 class GeneralSpider(Spider):
     name = 'general'
@@ -32,8 +33,11 @@ class GeneralSpider(Spider):
             soup = BeautifulSoup(page.text, 'html.parser')
             contents_key_value = {}
 
+            # URL
+            contents_key_value["url"] = kwargs["url"]
+
             # 제목 
-            contents_key_value["제목"] = soup.title.text 
+            contents_key_value["title"] = soup.title.text 
 
             # 내용 
             contents=[content for content in soup.stripped_strings]
@@ -46,19 +50,30 @@ class GeneralSpider(Spider):
                 del filtered_contents[147:290]
                 del filtered_contents[0:93] 
 
-            contents_key_value["주요내용"] = ",".join(filtered_contents)
+            contents_key_value["main_contents"] = ",".join(filtered_contents)
 
             # 목적 아래 위치에서만 아래 형식으로 데이터 확인이 가능함.
             if "https://www.assembly.go.kr/assm/assemact/council/council0101/assmSchCal/assemScanCalDetail.do" in kwargs["url"] :
                 
-                contents_key_value["목적"] = soup.find_all("tr","general")[0].contents[1].contents[1].contents[0]
+                contents_key_value["purpose"] = soup.find_all("tr","general")[0].contents[1].contents[1].contents[0]
                 subject_html = soup.find_all("tr", "subject")
                 for i in range(0, len(subject_html[0].find_all("th"))):
                     contents_key_value[subject_html[0].find_all("th")[i].text.strip()] = subject_html[0].find_all("td")[i].text.strip()
                 contents_key_value[subject_html[1].a.text] = subject_html[1].a['href']
 
-            # AWS 향으로 변경해 주어야함.
-            es = Elasticsearch(["https://gyujanggak-deployment.es.us-east-1.aws.found.io:9243"])
-            res = es.index(index='national-index', body=contents_key_value)
-            print("*************")
+            
+            # INSERT DATA TO Elastic search
+            config = configparser.ConfigParser()
+            config.read('/code/general-spider/general/spiders/example.ini')
+            app_search = AppSearch(
+                config['ELASTIC']['elastic_search'],
+                http_auth=config['ELASTIC']['private_key']
+            )
+
+            res = app_search.index_documents(
+                engine_name=config['ELASTIC']['engine_name'],
+                documents=[contents_key_value]
+            )
+
             print(res)
+
