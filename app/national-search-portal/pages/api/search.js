4c5b0804c6577c36e409043b726935352f0a9f5f
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-05-19 15:44:43 +0900

    Modify : call elastic search

--- app/national-search-portal/pages/api/search.js
+++ app/national-search-portal/pages/api/search.js
@@ -1,3 +1,24 @@
-export default function handler(req, res){
-    res.status(200).json({ data: 'Thank you for your interest...'})
+export default async function handler(req, res){
+
+    const result = await fetch(
+        'https://gyujanggak-deployment.ent.us-east-1.aws.found.io \
+        /api/as/v1/engines/gyujanggak-search-engine/search?query='
+        + encodeURIComponent(req.query.q),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer search-dyf6r2v7jndspp6r3fpgqh4r'
+            },
+            
+            method: 'GET'
+        }
+    )
+
+    const resultData = await result.json()
+    let data
+    if (resultData.results.length> 0){
+        console.log(resultData.results[0].main_contents)
+        data = resultData.results[0].title.raw
+    }
+    res.status(200).json({data: JSON.stringify(data)})
 }
