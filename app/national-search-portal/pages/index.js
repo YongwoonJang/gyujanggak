4abc997a5abfd188feaecbb229616963fb2a25ef
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-07-15 23:34:29 +0900

    Modify : Add static datas

--- app/national-search-portal/pages/index.js
+++ app/national-search-portal/pages/index.js
@@ -1,4 +1,4 @@
-import Layout, { siteTitle } from '../components/layout'
+import Layout from '../components/layout'
 import styles from '../styles/layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import urlStyles from '../styles/url.module.css'
@@ -10,7 +10,15 @@ import RequestFormAndResult from '../components/search-form'
 const name = '국회정보통합검색시스템'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = [
+    { "id": "InspectOfStateAdministration", "title": "국정감사정보", "url": "https://likms.assembly.go.kr/inspections/main.do" },
+    { "id": "NationalAssemblyLawInformation", "title": "국회법률정보", "url": "http://likms.assembly.go.kr/law/lawsNormInqyMain1010.do?mappingId=%2FlawsNormInqyMain1010.do&genActiontypeCd=2ACT1010" },
+    { "id": "NationalAssemblyMinuttes", "title": "국회회의록", "url": "http://likms.assembly.go.kr/record/index.jsp" },
+    { "id": "PersonalizedLegislative", "title": "의안 회의록", "url": "http://naph.assembly.go.kr/index.jsp" },
+    { "id": "OpenCongress", "title": "열려라 국회", "url": "http://watch.peoplepower21.org/home" },
+    { "id": "BudgetSettlementInfo", "title": "예결산정보시스템", "url": "http://likms.assembly.go.kr/bill/nafs/nafsList.do" },
+    { "id": "BillInfo", "title": "의안정보", "url": "http://likms.assembly.go.kr/bill/main.do" }
+  ]
   return {
     props: {
       allPostsData
