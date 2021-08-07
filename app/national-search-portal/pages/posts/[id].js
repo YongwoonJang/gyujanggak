416bd223153fb77785b32d0c95adf5d0601348aa
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-08-07 12:33:21 +0900

    modify : set formatting

---
+++ app/national-search-portal/pages/posts/[id].js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+
+export default function Post({data, contents}){
+    return (
+        <>
+            <div style={{paddingLeft:20+'px'}}>
+                <h1>{data.title}</h1>
+                <h2>{data.date}</h2>
+                <h3>{data.author.name}</h3>
+                <div>{contents}</div>
+            </div>
+        </>
+    )
+}
+
+export function getStaticPaths(){
+    const postNames = ["profile"]
+  
+    const params = postNames.map((postName) => ({
+        params: { id: postName  }
+    })) 
+
+    return {paths: params, fallback: 'blocking'}
+}
+
+export async function getStaticProps({ params }){
+    const fullPath = "public/posts/"+params.id
+    const fileContent = fs.readFileSync(fullPath)
+    const matterResult = matter(fileContent)
+    return {
+        props: {
+            data : matterResult.data,
+            contents : matterResult.content
+        },
+    }
+}
