7136d033c5b356c3db30ab72bcf7dc249d47b458
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-10-16 02:02:16 +0900

    Modify : identify double click

--- jyu-homepage/pages/posts/[id].js
+++ jyu-homepage/pages/posts/[id].js
@@ -68,16 +68,18 @@ export async function getStaticProps({ params }) {
 
 
 //Main function
-export default function Post({id, data, contents, comments}){
+export default function Post({id, data, contents, globalComments}){
     const content = parse(contents);
     
     //this line is used for comments
     const [lines, setLines] = useState("");
     const [defaultContents, setContents] = useState("Hello world");
     const [defaultAuthor, setAuthor] = useState("JYU");
+    const [comments, setComments] = useState(globalComments);
 
     const commentTableRef = useRef(null);
     const delDocIdRef = useRef(null);
+    
 
     //Editor area
     const editCommentBox = useRef(null);
@@ -122,15 +124,14 @@ export default function Post({id, data, contents, comments}){
         let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         let time = today.getHours() + "시 " + today.getMinutes() + "분";
         let dateTime = date + ' ' + time;
-        let seconds = today.getSeconds();
 
         let globalTime = today.getFullYear().toString()
             + (today.getMonth() + 1).toString()
             + today.getDate().toString()
             + today.getHours().toString()
             + today.getMinutes().toString();
-        let newId = globalTime + seconds + doc(collection(db, "gyujanggak")).id;
-        console.log(newId);
+            + today.getSeconds();
+        let newId = globalTime + doc(collection(db, "gyujanggak")).id;
 
         try {
             await setDoc(doc(db, "gyujanggak", newId), {
@@ -144,29 +145,48 @@ export default function Post({id, data, contents, comments}){
             console.error("Error adding document: ", e);
 
         }
-
-        comments.push({ "Author": author, "Content": contents, "Date": dateTime, "docId": newId });
-        setTable(comments);
-
+        //setComments([...comments, { "Author": author, "Content": contents, "Date": dateTime, "docId": newId }])
+        setComments(await readDatabase());
     }
 
     async function deleteRow(localDelDocId) {
         initializeApp(firebaseConfig);
         const db = getFirestore();
+        
         if (localDelDocId != null) {
-            await deleteDoc(doc(db, "gyujanggak", localDelDocId));
+            try {
+                await deleteDoc(doc(db, "gyujanggak", localDelDocId));
+                //setComments(comments.filter(items => items.docId != localDelDocId));
+                console.log("Document delete with ID: ", localDelDocId);
 
+            } catch (e) {
+                console.error("Error removing document: ", e);
+
+            }
         }
 
+        setComments(await readDatabase());
     }
 
-    //Component did mount
     useEffect(async () => {
+        console.log("init");
+        setComments(await readDatabase());
 
-        comments = await readDatabase();
+    },[]);
+
+    useEffect(() => {
+        console.log("setTable");
         setTable(comments);
-        console.log("useEffect");
+        
+    },[comments]);
+
+    //Component did mount
+    useEffect(() => {
+        console.log("setButton actions");
         commentTableRef.current.querySelectorAll('tr').forEach(e => e.addEventListener("click", function settingButton() {
+            console.log("button selected");
+            console.log(delDocIdRef.current.innerHTML);
+            console.log(this.children[2].innerHTML);
 
             if (delDocIdRef.current.innerHTML == this.children[2].innerHTML) {
                 this.style.backgroundColor = "white";
@@ -194,60 +214,10 @@ export default function Post({id, data, contents, comments}){
                 
             }
         }));
-    }, [comments]);
 
-    if(id == 'profile'){
-        let workHistory = "<table><tbody>";
-        const countOfRows = 5;
-        for(let i = countOfRows; i > 0; i--){
-            workHistory = workHistory 
-                        + "<tr><td>"
-                        + (countOfRows+1-i).toString()+". "+"<a href='" + data.workExperience[i]["URL"] + "'>"
-                        + data.workExperience[i]["Summary"]
-                        + "</a>("
-                        + data.workExperience[i]["Period"]
-                        + ")</td></tr>"
-                        + "<tr style='font-size: 0.9em; color: rgb(149, 143, 143)'><td>"
-                        + "&nbsp&nbsp" + data.workExperience[i]["Description"]
-                        + "</td></tr>"
-        }
-        workHistory = parse(workHistory+"</tbody></table>");
+    }, [lines]);
 
-        return (
-            <>
-                <div className={pageStyles.page}>
-                    <h1 className={pageStyles.profileTitle}>
-                        {parse(data.title)}
-                    </h1>
-                    <div className={pageStyles.profileImage}>
-                        <table>
-                            <tr>
-                                <td className={pageStyles.profileMotto}>
-                                    &nbsp;&nbsp;사소한 생활의 문제를 해결해주는 <br/>
-                                    &nbsp;&nbsp;기획자, Artist, programmer입니다.
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img layout="intrinsic"  width="400px" height="300px" src={"/images/profileImage.jpeg"} alt="My profile" />
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div className={pageStyles.profileWorkBox}>
-                        <div>
-                            {content}
-                        </div>
-                        <div>
-                            {workHistory}
-                        </div>
-                    </div>
-                </div>
-                <CopyRight />
-            </>
-        )
-
-    } else if (id == 'communication') {
+    if (id == 'communication') {
 
         const handleContentsChange = event => {
             event.preventDefault();
@@ -265,22 +235,10 @@ export default function Post({id, data, contents, comments}){
             event.preventDefault();
 
             if (delDocIdRef.current.innerHTML != "") {
-                for (let i = 0; comments.length; i++) {
-                    console.log("button for delete");
-                    console.log(comments);
-                    console.log(comments.length);
-                    if (comments[i].docId == delDocIdRef.current.innerHTML) {
-                        console.log(i);
-                        comments.splice(i, 1);
-                        break;
-                    }
-                }
-                setTable(comments);
                 deleteRow(delDocIdRef.current.innerHTML);
                 alert("성공적으로 삭제되었습니다.");
 
             } else {
-                console.log("insert is executed");
                 insertDatabase(defaultAuthor, defaultContents);
                 alert("성공적으로 저장되었습니다.");
 
@@ -288,12 +246,12 @@ export default function Post({id, data, contents, comments}){
 
             //reset all of status about delete and write.
             commentTableRef.current.querySelectorAll('tr').forEach(tr => {
-                tr.style.backgroundColor = "white"; 
-                tr.style.color = "black"; 
+                tr.style.backgroundColor = "white";
+                tr.style.color = "black";
                 regButton.current.style.display = "block";
                 delButton.current.style.display = "none";
                 delDocIdRef.current.innerHTML = "";
-            
+
             })
 
 
@@ -351,7 +309,58 @@ export default function Post({id, data, contents, comments}){
                 <CopyRight />
             </>
         )
-    }else if(id == 'profile-mgmt'){
+    }else if(id == 'profile'){
+        let workHistory = "<table><tbody>";
+        const countOfRows = 5;
+        for(let i = countOfRows; i > 0; i--){
+            workHistory = workHistory 
+                        + "<tr><td>"
+                        + (countOfRows+1-i).toString()+". "+"<a href='" + data.workExperience[i]["URL"] + "'>"
+                        + data.workExperience[i]["Summary"]
+                        + "</a>("
+                        + data.workExperience[i]["Period"]
+                        + ")</td></tr>"
+                        + "<tr style='font-size: 0.9em; color: rgb(149, 143, 143)'><td>"
+                        + "&nbsp&nbsp" + data.workExperience[i]["Description"]
+                        + "</td></tr>"
+        }
+        workHistory = parse(workHistory+"</tbody></table>");
+
+        return (
+            <>
+                <div className={pageStyles.page}>
+                    <h1 className={pageStyles.profileTitle}>
+                        {parse(data.title)}
+                    </h1>
+                    <div className={pageStyles.profileImage}>
+                        <table>
+                            <tr>
+                                <td className={pageStyles.profileMotto}>
+                                    &nbsp;&nbsp;사소한 생활의 문제를 해결해주는 <br/>
+                                    &nbsp;&nbsp;기획자, Artist, programmer입니다.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img layout="intrinsic"  width="400px" height="300px" src={"/images/profileImage.jpeg"} alt="My profile" />
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div className={pageStyles.profileWorkBox}>
+                        <div>
+                            {content}
+                        </div>
+                        <div>
+                            {workHistory}
+                        </div>
+                    </div>
+                </div>
+                <CopyRight />
+            </>
+        )
+
+    } else if(id == 'profile-mgmt'){
         let rows = "";
         const countOfRows = 14;
         for(let i = countOfRows; i > 0; i--){
