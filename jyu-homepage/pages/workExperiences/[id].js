cf3736751516b118d0c8bdf5ca6f0155d8e319a7
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-10-20 22:03:33 +0900

    Modify : 영업전산 프로젝트 계약 관리 추가

--- jyu-homepage/pages/workExperiences/[id].js
+++ jyu-homepage/pages/workExperiences/[id].js
@@ -1,26 +1,7 @@
 import fs from 'fs'
-
-import { useState, useRef, useCallback } from 'react'
-
 import matter from 'gray-matter'
 import parse from 'html-react-parser'
-
-import Link from 'next/link'
 import pageStyles from '/styles/page.module.scss'
-import profileDivTableStyles from '/styles/profileTable.module.scss'
-
-import CopyRight from '../../components/copyRight'
-
-import React, { useEffect } from 'react'
-import { vsSource, fsSource, createShader, createProgram, initBuffer, render } from '/components/drawingTheScene'
-import { drawScene } from '../../components/drawingTheScene'
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from '../../components/firebase';
-
-import { setDoc, deleteDoc, collection } from "firebase/firestore";
-import { doc, getDocs } from "firebase/firestore";
 
 //Static function
 export function getStaticPaths() {
@@ -36,8 +17,8 @@ export function getStaticPaths() {
 export async function getStaticProps({ params }) {
     //It only process one time
 
-    const fullPath = "public/workExperience/" + params.id + ".md"
-    let matterResult = {"data" : {"title":"Ready"},"content" : "내용 준비 중입니다."};
+    const fullPath = "public/workExperiences/" + params.id + ".md"
+    let matterResult = {"data" : {"title":"Ready"},"content" : "내용 준비 중입니다. 현재 영업 전산 프로젝트 계약 관리만 내용이 있습니다."};
 
     try{
         const fileContent = fs.readFileSync(fullPath)
