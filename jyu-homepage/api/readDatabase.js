0cd610ac6d1b90b2ea2354fb476c2524d6fb4014
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-10-25 20:39:36 +0900

    Test

--- jyu-homepage/api/readDatabase.js
+++ jyu-homepage/api/readDatabase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 
 }
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
