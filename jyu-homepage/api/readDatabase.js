05dfd886d9f77ec60acc7d9765cfb1da0d119456
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-12-12 02:46:48 +0900

    fix : bug 'require to import'

--- jyu-homepage/api/readDatabase.js
+++ jyu-homepage/api/readDatabase.js
@@ -1,5 +1,5 @@
-const { initializeApp } = require("firebase/app");
-const { getFirestore, collection, getDocs } = require("firebase/firestore");
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
