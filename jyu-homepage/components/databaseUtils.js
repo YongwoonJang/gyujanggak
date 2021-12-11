82926aeff24ec0f2a4ec4521a62baebd003137be
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-12-11 14:40:10 +0900

    delete : for prd

--- jyu-homepage/components/databaseUtils.js
+++ jyu-homepage/components/databaseUtils.js
@@ -59,15 +59,15 @@ export function signIn(){
         const app = initializeApp(firebaseConfig);
         const auth = getAuth(app);
 
-        auth.then((auth) => {
-            auth.verifyIdToken(result)
-            .then((decodedToken) => {
-                console.log(decodedToken);
-            })
-            .catch((error) =>{
-                console.log(error);
-            })
-        })
+        // auth.then((auth) => {
+        //     auth.verifyIdToken(result)
+        //     .then((decodedToken) => {
+        //         console.log(decodedToken);
+        //     })
+        //     .catch((error) =>{
+        //         console.log(error);
+        //     })
+        // })
 
     });
 
