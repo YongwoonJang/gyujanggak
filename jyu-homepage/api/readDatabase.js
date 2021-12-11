be8fb26bdbbbc5f3ed486955f47bb516006bcbcf
royalfamily89@gmail.com <royalfamily89@gmail.com>
2021-12-12 02:38:11 +0900

    fix : bug

--- jyu-homepage/api/readDatabase.js
+++ jyu-homepage/api/readDatabase.js
@@ -1,7 +1,5 @@
 const { initializeApp } = require("firebase/app");
-const { getDatabase, ref, onValue } = require("firebase/database");
 const { getFirestore, collection, getDocs } = require("firebase/firestore");
-const { getAuth, signInWithEmailAndPassword, signOut } = require("firebase/auth");
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
@@ -13,21 +11,14 @@ const firebaseConfig = {
     databaseURL: process.env.DATABASE_URL
 }
 
-const identification = {
-    "user": process.env.USER_ID,
-    "code": process.env.CODE
-}
-
 
 module.exports = async (req, res) => {
     const fullURL = new URL(req.url, `http://${req.headers.host}`);
     let name = fullURL.searchParams.get('name');
     const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
     
     let data = [];
-    
-    await signInWithEmailAndPassword(auth, identification["user"], identification["code"]);
+
     const db = getFirestore(app);
     const gyujanggakRef = collection(db, name);
     const gyujanggakSnapshot = await getDocs(gyujanggakRef);
