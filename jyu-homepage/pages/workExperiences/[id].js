e7e2f60b8da073bc69bcb7b8723d9822a9fd59c6
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-04-27 00:48:32 +0900

    style : change work experience style.

--- jyu-homepage/pages/workExperiences/[id].js
+++ jyu-homepage/pages/workExperiences/[id].js
@@ -138,9 +138,7 @@ function commonHistoryCard(){
                         &nbsp;
                     </div>
                     <div ref={leafBorn} className={profileDivTableStyles.profileTreeBornLeaf}>
-                        -- 한글<br/>
-                        -- 1989년 1월 인천 출생<br /><br />
-                        -- English<br />
+                        -- 1989년 1월 인천 출생<br />
                         -- Born at Jan. 1989<br />
                     </div>
                 </div>
@@ -148,10 +146,8 @@ function commonHistoryCard(){
                 <div className={profileDivTableStyles.profileTreeBGItem}>
                     <div ref={leafSchool} className={profileDivTableStyles.profileTreeSchoolLeaf}>
                         <p>
-                        한글 --<br/>
                         2004년 3월, 인천고등학교 입학 --<br />
                         2007년 3월, 건국대학교 입학 --<br /><br />
-                        English --<br/>
                         March. 2004. Entrance to Inchon High school --<br/>
                         March. 2007. Entrance to Konkuk University --<br/>
                         </p>
@@ -179,7 +175,8 @@ function commonHistoryCard(){
                     </div>
                     <div ref={leafOfficer} className={profileDivTableStyles.profileTreeOfficerLeaf}>
                         <p>
-                            ---------- <br /> 2011년 임관<br />(대한민국 통신장교)
+                            -- 2011년 임관 (대한민국 통신장교) <br/>
+                            -- March 2011. Commissione to Officer <br/>
                             <div>
                                 <Image src={'/images/CommissionedAsAOfficer.png'} width={250} height={200} />
                             </div>
@@ -190,7 +187,8 @@ function commonHistoryCard(){
                 <div className={profileDivTableStyles.profileTreeBGItem}>
                     <div ref={leafEntry} className={profileDivTableStyles.profileTreeJobLeaf}>
                         <p>
-                            2013년 7월 KT 입사--<br /> 
+                            2013년 7월 KT 입사 --<br />
+                            July. 2013. Entrance to KT --<br/>
                         </p>
                     </div>
                     <div className={profileDivTableStyles.profileTreeStem}>
@@ -209,11 +207,25 @@ function commonHistoryCard(){
 // History of KT.
 //               KT
 //                | 
-//                | -- 우리말
+//                | -- 한글
 //                | -- 2013년 11월 ~ 2014년 5월
 //                | -- 통신사 Business Support System의 SW 라이선스 관리
 //
 function ktHistoryCard(){
+    const leafBSS = useRef(null);
+    const leafRater = useRef(null);
+    // const leafOfficer = useRef(null);
+    // const leafEntry = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("scroll", function () {
+            if (document.documentElement.scrollTop > 900){
+                leafBSS.current.style.marginTop =+ document.documentElement.scrollTop / 25 + 'px';
+                leafRater.current.style.marginTop =- document.documentElement.scrollTop / 15 + 'px';
+            }
+        });
+    }, []);
+
     return(
         <>
             <div>
@@ -228,15 +240,13 @@ function ktHistoryCard(){
                     <div className={profileDivTableStyles.jobTreeStem}>
                         &nbsp;
                     </div>
-                    <div>
+                    <div ref={leafBSS}>
                         <p>
                             <div className={profileDivTableStyles.detailedWorkExperienceStatement}>
-                            -- 우리말 <br />
                             -- 2013년 11월 ~ 2014년 5월 <br />
                             -- 통신사 Business support system의 SW 라이선스 관리 <br />
                             -- JIRA, Sharepoint Operating 운영 능력 향상 <br /><br />
-                            -- English  <br />
-                            -- Nov.2013 ~ May.2014 <br/>
+                            -- Nov. 2013 ~ May. 2014 <br/>
                             -- Telco. BSS Resource and Software License management <br/>
                             -- Earned skill JIRA, Sharepoint Operating skill <br/>
                             </div>
@@ -247,13 +257,11 @@ function ktHistoryCard(){
                     </div>
                 </div> 
                 <div className={profileDivTableStyles.profileTreeBGItem}>
-                    <div className={profileDivTableStyles.profileTreeWorkLeftLeaf}>
+                    <div ref={leafRater} className={profileDivTableStyles.profileTreeWorkLeftLeaf}>
                         <div className={profileDivTableStyles.detailedWorkExperienceStatement}>
-                            우리말 --<br />
                             2014년 6월 ~ 2018년 11월 --<br />
                             통신사 유/무선 Rater system의 계약 관리 --<br />
                             이스라엘 업체와의 계약 및 협상 능력 향상 --<br/> <br/>
-                            English --<br/>
                             June 2014 ~ Nov 2018 --<br/>
                             통신사 BSS Rater(Billing) 서비스 운영 및 계약 관리 --<br/>
                             Earned skill : Contract with offshore (Israel) --<br/>
@@ -278,11 +286,9 @@ function ktHistoryCard(){
                     <div>
                         <p>
                             <div className={profileDivTableStyles.detailedWorkExperienceStatement}>
-                                -- 우리말 <br/>
                                 -- 2018년 11월 ~ 2022년 3월 31일 <br />
                                 -- KT Cloud 포탈 BA 및 Cloud billing system BA <br />
                                 -- REST API documentation, SaaS architecturing 능력 향상 <br /><br />
-                                -- English <br/>
                                 -- Nov. 2018 ~ March. 2022
                                 -- KT Cloud portal BA / Cloud billing system BA <br />
                                 -- Earned skill : REST API documentation, SaaS architecturing<br/>
@@ -319,17 +325,15 @@ function ktcloudHistoryCard(){
             <div className={profileDivTableStyles.ktcloudProfileTreeBGItem}>
                 <div className={profileDivTableStyles.ktcloudProfileTreeWorkLeftLeaf}>
                     <div className={profileDivTableStyles.ktcloudDetailedWorkExperienceStatement}>
-                        우리말 -- <br/>
                         2022년 4월 1일 ~ 현재 -- <br />
                         SaaS 서비스 Product Owner(KT Cloud BizMeet, BizOffice, bizplay) --<br />
                         SaaS Marketing, Architecturing, CSAP 인증 능력 향상 --<br /><br />
-                        English --<br/>
                         Nov. 2018 ~ March. 2022 --<br/>
                         KT Cloud portal BA / Cloud billing system BA --<br />
                         Earned skill : REST API documentation, SaaS architecturing --<br/>
                     </div>
                     <div>
-                        <Image src={'/images/ktToKtcloudNew.jpg'} layout={"intrinsic"} width={600} height={350} />
+                        <Image src={'/images/ktToKtcloudNew.jpg'} layout={"intrinsic"} width={600} height={350} alt="Loading" />
                     </div>
                 </div>
                 <div className={profileDivTableStyles.jobTreeStem}>
@@ -337,9 +341,6 @@ function ktcloudHistoryCard(){
                 </div>
                 <div className={profileDivTableStyles.profileTreeBornLeaf}>
                 </div>
-            </div>
-            <div>
-                <Image src={'/images/background/GreenLightBand(bottom).png'} layout="responsive" width={1200} height={10} />
             </div>
         </>
     )
