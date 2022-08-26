eb22fb917bdd5b84cb3f4e06b59ea17b578ab6cc
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-08-26 19:01:01 +0900

    fix: fix regex for email address

--- jyu-homepage/pages/workExperiences/[id].js
+++ jyu-homepage/pages/workExperiences/[id].js
@@ -17,7 +17,7 @@ let stemOfKT = <div className={profileDivTableStyles.profileTreeBGItem}><div>&nb
 
 
 function sendToOwner(mailAddr, contents) {
-    let mailRegExp = /^[a-z0-9\.]+@[a-z0-9]+\.[a-z]+$/i
+    let mailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
     if(!mailAddr.match(mailRegExp)){
         
