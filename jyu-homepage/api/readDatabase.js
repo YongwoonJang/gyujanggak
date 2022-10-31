3c048f59d0b2e744e5fa04d43682ee1fc37b5ac9
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-10-31 19:05:40 +0900

    style : delete unused config data

--- jyu-homepage/api/readDatabase.js
+++ jyu-homepage/api/readDatabase.js
@@ -3,28 +3,21 @@ const { getFirestore, collection, getDocs } = require("firebase/firestore");
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
-    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    // appId: process.env.API_ID,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
-    // storageBucket: process.env.STORAGE_BUCKET,
-    // databaseURL: process.env.DATABASE_URL
+    
 }
 
 
 module.exports = async (req, res) => {
     const fullURL = new URL(req.url, `http://${req.headers.host}`);
     let name = fullURL.searchParams.get('name');
-    console.log(name);
-    
     let data = [];
 
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     const gyujanggakRef = collection(db, name);
     const gyujanggakSnapshot = await getDocs(gyujanggakRef);
-
-    console.log(gyujanggakSnapshot);
 
     gyujanggakSnapshot.forEach((doc) => {
         let tempObject = doc.data();
