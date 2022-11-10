dad69699116e6c6ed9fd9f643ae9b983a51fcd59
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-11-10 21:13:35 +0900

    style: add indent

--- jyu-homepage/components/layout.js
+++ jyu-homepage/components/layout.js
@@ -7,19 +7,19 @@ export const siteTitle = 'JYU homepage'
 export default function Layout({children}) {
   return (
     <>
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="JYU homepage"
-          content="장용운을 설명합니다."
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      {children}
-    </div >
-    <CopyRight/>
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="JYU homepage"
+            content="장용운을 설명합니다."
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        {children}
+      </div >
+      <CopyRight/>
     </>
   )
 }
