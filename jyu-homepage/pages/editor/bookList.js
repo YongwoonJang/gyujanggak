ccfa73b3512958f8f2d011b7dcbb3454d255e78c
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-11-27 08:54:17 +0900

    fix: bug.

--- jyu-homepage/pages/editor/bookList.js
+++ jyu-homepage/pages/editor/bookList.js
@@ -3,27 +3,20 @@ import { useState, useEffect } from 'react';
 
 export default function BookList(props){
     
-    const [contents, setContents] = useState(null);
-    let contentsList = [];
+    let contents = [];
 
-    useEffect(()=>{
-        console.log("useEffect in book list")
-        console.log(props.value);
-        if (props.data !== undefined) {
-            console.log("Inner useEffect function")
-            props.value.forEach((data) => {
-                contentsList.push(
-                    <tr className={bookListStyle.row}>
-                        <td>{data.title}</td>
-                        <td className={(data.currentStatus !== "독서중" ? bookListStyle.green : bookListStyle.red)}>{(data.currentStatus !== "독서중" ? "대출가능" : "독서 중")}</td>
-                        <td>{'최근 "' + data.loanDate + '"에 대출'}</td>
-                    </tr>
-                )
-            })
-
-            setContents(contentsList);
-        }
-    },props.value)
+    if (props.value !== undefined) {
+        console.log("Inner useEffect function")
+        props.value.forEach((data) => {
+            contents.push(
+                <tr className={bookListStyle.row}>
+                    <td>{data.title}</td>
+                    <td className={(data.currentStatus !== "독서중" ? bookListStyle.green : bookListStyle.red)}>{(data.currentStatus !== "독서중" ? "대출가능" : "독서 중")}</td>
+                    <td>{'최근 "' + data.loanDate + '"에 대출'}</td>
+                </tr>
+            )
+        })
+    }
 
     return(
         <>
