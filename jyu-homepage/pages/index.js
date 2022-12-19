0e52ba0d851c13a31591de66df53c496fb30c07e
royalfamily89@gmail.com <royalfamily89@gmail.com>
2022-12-20 07:59:15 +0900

    feature: apply priority.

--- jyu-homepage/pages/index.js
+++ jyu-homepage/pages/index.js
@@ -24,6 +24,7 @@ export default function Home() {
                   height={300}
                   width={300}
                   alt={title}
+                  priority
                 />
               </a>
             </Link>
