e7cf360e8a0efc2cc34bbceeac84171727792b4f
yongwoon Jang <royalfamily89@gmail.com>
2023-07-24 02:12:57 +0900

    fix: bug

--- jyu-homepage/pages/main.js
+++ jyu-homepage/pages/main.js
@@ -162,7 +162,7 @@ export default function Main(){
                     <div>
                         <input 
                             onFocus={()=>{setFocus(true)}} 
-                            onFocusIn={()=>{setFocus(true)}}
+                            onFocusin={()=>{setFocus(true)}}
                             onBlur={()=>{setFocus(false)}} 
                             onChange={promptActionHandler} 
                             className={pageStyles.comBannerGroupInput} 
